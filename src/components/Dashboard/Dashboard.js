5a95b08a699ed06025aa691618f0f4ef2baddb69
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-04-05 16:32:56 +0600

    added file after finishing dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import { Area, Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ];
     return (
-        <div>
-            <h1>this dashboard</h1>
+        <div className='chart-container'>
+            <div>
+                <LineChart width={400} height={300} data={data}>
+                    <Line dataKey={'sell'}></Line>
+                    <Line dataKey={'revenue'}></Line>
+
+                    <XAxis dataKey={'month'}></XAxis>
+                    <Tooltip></Tooltip>
+                    <YAxis dataKey={'revenue'}></YAxis>
+
+                </LineChart>
+            </div>
+            <div>
+                <PieChart width={730} height={250}>
+                    <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" innerRadius={50} outerRadius={90} fill="#82ca9d" label />
+                    <Tooltip></Tooltip>
+                </PieChart>
+            </div>
+            <div>
+                <BarChart width={730} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="revenue" fill="#8884d8" />
+                    <Bar dataKey="sell" fill="#82ca9d" />
+                </BarChart>
+            </div>
+            <div>
+                <ComposedChart width={730} height={250} data={data}>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Area type="monotone" dataKey="sell" fill="#8884d8" stroke="#8884d8" />
+                    <Bar dataKey="investment" barSize={20} fill="#413ea0" />
+                    <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+                </ComposedChart>
+            </div>
         </div>
     );
 };
